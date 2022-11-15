6fd68019ee3ad2df153ea751d27833c672a55be2
Michael Mason <mikemason930@gmail.com>
2022-11-15 13:44:56 -0500

    Fix default sort order to be descending

--- controller/main.php
+++ controller/main.php
@@ -155,7 +155,7 @@ class main
         global $table_prefix;
         
         $sort_type = $this->request->variable('sort_type', 'pt');
-        $sort_order = $this->request->variable('sort_order', 'a');
+        $sort_order = $this->request->variable('sort_order', 'd');
 
         $sort_type_sql = $this->sort_type_map[$sort_type] ?? 'max(p.post_time)';
         $sort_order_sql = $sort_order == 'd' ? 'DESC' : 'ASC';
